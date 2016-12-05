b5b55a7513f07b06f832cc3501ef11e937613832
daveboden <dave@daveboden.com>
2016-12-05 11:50:30 +0000

    Finds a candidate hide block using getBlocks() for efficiency rather than checking lots of blocks individually.

--- hidenseek.py
+++ hidenseek.py
@@ -8,13 +8,18 @@
 
 import mcpi.minecraft as minecraft
 import mcpi.block as block
+from mcpi.connection import RequestError
 from mcpi.vec3 import Vec3
 import time
 from random import randint, randrange
 from time import sleep
 
 mc = minecraft.Minecraft.create(address="localhost")
-pos = mc.player.getTilePos()
+try:
+    pos = mc.player.getTilePos()
+except RequestError:
+    print("Error: Can't find a player to work with. Are you in the Minecraft world?")
+    exit(2)
 
 #Pick a spot between 50 and 100 blocks away in the x and z directions
 #can be either positive or negative
@@ -27,14 +32,33 @@ hideZ = pos.z + offsetZ
 print("Finding a y-coordinate at candidate x=%d, z=%d" % (hideX, hideZ))
 
 #Find the surface level at that location.
+#Cover between 255 and -255 as a y-coordinate due to the world spawn point
+#could be anywhere between 0 and 255 y-coordinate.
+
+columnBlockData = mc.getBlocks(hideX, 255, hideZ, hideX, -255, hideZ)
+
+#We get Air(0) returned for blocks that don't exist and the highest blocks are at the end of the array
+assert len(columnBlockData) == 511
 
 #Start at the top of the world and work down until you meet
 #the ground (or water or leaves)
-for candidateY in range(255, -255, -1):
-    if(mc.getBlock(hideX, candidateY, hideZ) != block.AIR):
+for candidateYArray in range(len(columnBlockData) - 1, 0, -1):
+    if(columnBlockData[candidateYArray] != block.AIR.id):
+        candidateY = candidateYArray - 255
         break
 
-if candidateY < -245:
+        # Here's the more chatty way to do it; asking the value of each block
+        # for candidateY in range(255, -255, -1):
+        #     if (mc.getBlock(hideX, candidateY, hideZ) != block.AIR):
+        #         break
+
+assert candidateY != None #We should have got a candidate value
+
+if candidateYArray <= 5:
+    print("Please try again to find a better hiding place away from the lowest possible y-coordinate")
+    exit(1)
+
+if block.BEDROCK.id in columnBlockData[candidateYArray-5:candidateYArray]:
     print("Please try again to find a better hiding place away from the bedrock")
     exit(1)
 
@@ -61,16 +85,21 @@ while True:
         mc.setBlock(mc.player.getTilePos() + (Vec3(0, 1, 0)), block.LAVA_FLOWING)
         break #Break out of loop if time is up
     if mc.getBlock(hideX, hideY, hideZ) != block.DIAMOND_BLOCK.id:
-        mc.postToChat("Well done; you mined the diamond!")
+        winningMessage = "Well done; you mined the diamond!"
+        winningMessage2 = "You've earned %d points" % timeLeftSecondsRounded
+        mc.postToChat(winningMessage)
+        mc.postToChat(winningMessage2)
+        print(winningMessage)
+        print(winningMessage2)
         break #Break out of loop if the block has been found and mined
     if timeLeftSecondsRounded % 10 == 0 and displayUpdatedForTime != timeLeftSecondsRounded:
         displayUpdatedForTime = timeLeftSecondsRounded
         print("%d seconds left" % timeLeftSecondsRounded)
 
     #Sleep for a short while to avoid this being a tight loop
-    sleep(0.1)
+    sleep(0.4)
 
-    if ticks % 20 == 0:
+    if ticks % 5 == 0:
         userposition = mc.player.getTilePos()
         mc.postToChat("%d seconds left" % timeLeftSecondsRounded)
         blockMessage = "Block is at: %4d, %4d, %4d" % (hideX, hideY, hideZ)
@@ -80,4 +109,4 @@ while True:
         print(blockMessage)
         print(userPosMessage)
 
-    ticks = ticks + 1
+    ticks += 1
